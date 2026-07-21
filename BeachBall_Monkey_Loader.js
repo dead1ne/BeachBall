4b350837eb58790a385b281c312034c1efdb2f5d
dead1ne <dead1ne@gmail.com>
2026-07-21 13:28:11 -0400

    Update script url, match crashsnowdon.com

--- BeachBall_Monkey_Loader.js
+++ BeachBall_Monkey_Loader.js
@@ -4,10 +4,8 @@
 // @version      1.1
 // @description  Beachball autoloader for Sandcastle Builder - SSL patch.
 // @author       seraphina985
-// @match        http://castle.chirpingmustard.com/castle.html
-// @match        http://castle.chirpingmustard.com/classic.html
-// @match        https://castle.chirpingmustard.com/castle.html
-// @match        https://castle.chirpingmustard.com/classic.html
+// @match        https://crashsnowdon.com/sandtest/castle.html
+// @match        https://crashsnowdon.com/sandtest/classic.html
 // @grant        none
 // ==/UserScript==
 
@@ -16,7 +14,7 @@ setTimeout(loadBB, 5000);
 function loadBB() {
     var js = document.createElement('script');
     js.setAttribute('type', 'text/javascript');
-    js.setAttribute('src', 'https://seraphina985.github.io/BeachBall/BeachBall.js');
+    js.setAttribute('src', 'https://dead1ne.github.io/BeachBall/BeachBall.js');
     document.head.appendChild(js);
 }
 
