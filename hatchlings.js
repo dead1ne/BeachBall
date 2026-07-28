773e734076f425b9359671945ce9a5bfd0d335dc
dead1ne <dead1ne@gmail.com>
2026-07-28 14:31:46 -0400

    impl hatchlings automation, notify filter

---
+++ hatchlings.js
@@ -0,0 +1,58 @@
+
+// if(Molpy.Spend({Bonemeal: Molpy.EggCost().bonemeal, Princesses: Molpy.EggCost().princess}))Molpy.Add('Eggs',1);
+// !Molpy.Got('Eggs') || Molpy.Boosts['Eggs'].Level
+
+function findLowestDragonFreeNP() {
+    var maxNP = Math.abs(Molpy.highestNPvisited);
+    for (var i = 1; i <= maxNP; i++) {
+        var npd = Molpy.NPdata[i];
+        if (!npd || npd.amount == 0) return i;
+    }
+    return 0;
+}
+
+function doHatchlings() {
+    me = Molpy.Boosts['Hatchlings'];
+    if (!me.Level) return;
+    for (var cl in me.clutches) {
+        if (me.age[cl] < 1000) {
+            if (Molpy.Boosts.DQ.overallState != 3) {
+                var np = findLowestDragonFreeNP();
+                if (np == 0) {
+                    console.log('No dragon free NPs');
+                    continue;
+                }
+                var oldNP = Molpy.newpixNumber;
+                Molpy.TTT(np, 1);
+                Molpy.DragonFledge(cl);
+                Molpy.TTT(oldNP, 1);
+                // TODO fix return hack to deal with clutches mutation
+                return;
+                // or
+                /* if (Molpy.Got('Cryogenics')) Molpy.DragonsToCryo(cl) */
+            }
+            else {
+                // partying?
+            }
+        }
+        else if (me.diet[cl]) {
+            // maturing ready to fledge in (me.age[cl] - 1000)
+        }
+        else {
+            var cls = me.clutches[cl];
+            if (Molpy.Has('Goats', cls * 1e6)) {
+                Molpy.DragonFeed(cl, 1);
+                // TODO fix return hack to deal with clutches mutation
+                return;
+            }
+            /* if (Molpy.Has('Princesses', cls * 10)) {
+                Molpy.DragonFeed(cl, 3)
+            }*/
+        }
+    }
+}
+
+function doHatchlingsCB() {
+    doHatchlings();
+    setTimeout(doHatchlingsCB, 10000);
+}
---
+++ notify.js
@@ -0,0 +1,75 @@
+globalThis.DeadExt ??= {};
+
+const DE = globalThis.DeadExt;
+
+DE.notify_ignore = [
+    'You accidentally slip through the temporal rift!',
+    'Ninja Forgiven',
+    'Ninja Unstealthed',
+
+    'One less lock on the crate',
+    'One less lock on the vault',
+    'You wonder when you are',
+    'You don\'t notice anything especially notable.',
+    'ONG!',
+    'Boost Locked: Temporal Rift',
+    'VITSSÅGEN, JA!',
+    'Not Lucky!',
+    'Lucky Twin!',
+    'You are doubly not Lucky!',
+    'You are not Lucky (which is good)',
+    'Lightning struck the same place twice: 10% power bonus!',
+    'Boost Unlocked: Monty Haul Problem',
+    'Blast Furnace in Operation!',
+    'The DoRD has produced:',
+    'Your Ranger caught a wild logicat!',
+    'Panther Poke: Keeps the Caged Logicat awake a little longer.',
+    'Only 2mNP of discounts remain!',
+    'Boost Locked: Affordable Swedish Home Furniture',
+    'Boost Unlocked: Locked Vault',
+    'Boost Unlocked: Vault Key',
+    'Boost Locked: Locked Vault',
+    'Boost Locked: Vault Key',
+    'Boost Locked: Glassed Lightning',
+    'The DoRD has provided:',
+    'Affordable Swedish Home Furniture: 60% off all items for 10mNP',
+    'Boost Locked: Blitzing',
+];
+
+DE.notify_ignore_re = [
+    /^Time Travel successful! Welcome to NewPix/,
+    /^Warped back in time/,
+
+    /^Great Scott/,
+    /^\d+ answers? correct/,
+    /^You already have this Discovery/,
+    /^During the last 100 digs, the dragons have found: [0-9.]+ (Copper|Silver|Gold|Diamonds) and [0-9.]+ (Copper|Silver|Gold|Diamonds)/,
+    /^Activating Factory Automation/,
+    /^Blitzing:/,
+    /^Glassed Lightning:/,
+    /^You found .+ flux crystals.$/,
+];
+
+DE.Molpy_Notify ??= Molpy.Notify;
+
+DE.NotifyTypo = function(text) {
+    if (Molpy.options.typo) return text; // typo = 1 = off
+
+	var squirpy = eternalf[gainned];
+	if(squirpy) return squirpy;
+
+    return text;
+};
+
+Molpy.Notify = function(text, importance, nolog) {
+    for (var i = 0; i < DE.notify_ignore.length; i++) {
+        if (DE.notify_ignore[i] == text) return;
+    }
+
+    for (var i = 0; i < DE.notify_ignore_re.length; i++) {
+        if (DE.notify_ignore_re[i].test(text)) return;
+    }
+
+    console.log(text);
+    DE.Molpy_Notify(text, importance, nolog);
+};
