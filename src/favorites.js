d883ec6828109f71ac5582d37042f70888863144
dead1ne <dead1ne@gmail.com>
2026-08-04 11:31:26 -0400

    skel favorites

---
+++ src/favorites.js
@@ -0,0 +1,10 @@
+globalThis.DeadExt ??= {};
+
+const DE = globalThis.DeadExt;
+
+if(typeof(Storage) !== 'undefined') {
+    
+}
+for (var i in  Molpy.Boosts.favs.FavesList) {
+  console.log(Molpy.BoostsById[Molpy.Boosts.favs.FavesList[i]].name);
+}
